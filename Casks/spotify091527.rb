8d5294db570e28afa7bb7af7c37b52b5d4ba9081
Marcin Michalowski <marcin@stick.pl>
2015-05-12 22:41:26 +0200

    Add Spotify 0.9.15.27

---
+++ Casks/spotify091527.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'spotify091527' do
+  version '0.9.15.27'
+  sha256 '83a569463c7bafd2947471e0657f20f30f29eed2e7c69b31545c09999e8c1417'
+
+  url 'http://fs34.filehippo.com/1954/8166a51068a7433b85b5ef82d271e450/Spotify.dmg'
+  name 'Spotify'
+  homepage 'https://www.spotify.com'
+  license :gratis
+
+  app 'Spotify.app'
+end

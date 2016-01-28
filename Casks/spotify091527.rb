5082b4d7ef4ca8302991691dc9769baa0a1e79ea
Marcin Michalowski <marcin@stick.pl>
2016-01-28 12:51:33 +0100

    Update Spotify formula

--- Casks/spotify091527.rb
+++ Casks/spotify091527.rb
@@ -1,8 +1,8 @@
-cask :v1 => 'spotify091527' do
+cask 'spotify091527' do
   version '0.9.15.27'
   sha256 '83a569463c7bafd2947471e0657f20f30f29eed2e7c69b31545c09999e8c1417'
 
-  url 'http://fs34.filehippo.com/1954/8166a51068a7433b85b5ef82d271e450/Spotify.dmg'
+  url 'https://www.dropbox.com/s/3bsbi5b3q4ti0yf/Spotify.dmg?dl=1'
   name 'Spotify'
   homepage 'https://www.spotify.com'
   license :gratis
@@ -11,6 +11,7 @@ cask :v1 => 'spotify091527' do
 
   caveats <<-EOS.undent
     Block automatic updates:
+      mkdir -p ~/Library/Application\ Support/Spotify/
       touch ~/Library/Application\\ Support/Spotify/Spotify_new.archive
       touch ~/Library/Application\\ Support/Spotify/Spotify_new.archive.sig
       chflags uchg ~/Library/Application\\ Support/Spotify/Spotify_new.archive

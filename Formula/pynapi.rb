19a65dc4a36d74988b2af532c9855229937155b4
Marcin Michalowski <marcin@stick.pl>
2022-01-07 00:03:57 +0100

    Update pynapi

--- Formula/pynapi.rb
+++ Formula/pynapi.rb
@@ -1,11 +1,9 @@
-require "formula"
-
 class Pynapi < Formula
   desc "Find video files and download matching subtitles from napiprojekt/napisy24 server"
   homepage "https://github.com/pld-linux/pynapi"
 
-  url "https://github.com/pld-linux/pynapi/archive/auto/th/pynapi-0.20-3.tar.gz"
-  sha256 "263f3a85a18bed43aa72c577036acdbd5bc03e4b8aa1c3f924d2899c8e6ced8e"
+  url "https://github.com/pld-linux/pynapi/archive/refs/tags/auto/th/pynapi-0.21-1.tar.gz"
+  sha256 "9432024f1ef7bd05d9d47ad4889a6be40dbf909752bb7eec0f6bdc485352363d"
 
   head "https://github.com/pld-linux/pynapi.git"
 
